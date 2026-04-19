a154bfff549459d489c599d59cee0c2acb41e313
pcxlab <pcxlabofficial@outlook.com>
2026-04-19 12:05:57 +0530

    Restructured modules with versioning (1.0.0)

---
+++ TEST_Notes.ps1
@@ -0,0 +1,56 @@
+﻿Import-Module PCXLab.Excel -Force
+Import-Module PCXLab.Core -Force
+Get-Command -Module PCXLab.Excel
+Get-Command -Module PCXLab.Core
+
+Remove-Module PCXLab.Excel -Force
+Remove-Module PCXLab.Core -Force
+Get-Command -Module PCXLab.Excel
+Get-Command -Module PCXLab.Core
+
+Get-Module PCXLab.Excel -ListAvailable
+Get-Module PCXLab.Core -ListAvailable
+
+Import-Module PCXLab.Excel -Force
+
+
+cd C:\Projects\Automation\Modules
+
+Import-Module .\PCXLab.Core
+Get-Command -Module PCXLab.Core
+
+Import-Module .\PCXLab.Excel
+Get-Command -Module PCXLab.Excel
+
+Get-Module PCXLab.Excel -ListAvailable
+Get-Module PCXLab.Core -ListAvailable
+
+
+$env:PSModulePath -split ";"
+
+$env:PSModulePath += ";C:\Projects\Automation\Modules"
+$env:PSModulePath -split ";"
+
+$env:PSModulePath += ";C:\Projects\Automation\Modules"
+
+Get-Module PCXLab.Excel -ListAvailable
+Get-Module PCXLab.Core -ListAvailable
+
+$env:PSModulePath += ";C:\Projects\Automation\Modules"
+
+Get-Module PCXLab.Excel -ListAvailable
+Get-Module PCXLab.Core -ListAvailable
+
+<#
+
+🧠 When do you need permanent setup?
+
+Only if you want:
+
+👉 Use modules anywhere (outside your script)
+
+Then you can install modules into:
+
+C:\Users\Administrator\Documents\WindowsPowerShell\Modules
+
+#>
